7fa8376b20451c39bb04065c8be2503412b355d0
CentOS <centos@centos8.linuxvmimages.local>
2024-04-19 21:51:46 +0530

    fix: edited 4/2

--- Lab4/untrash.sh
+++ Lab4/untrash.sh
@@ -19,6 +19,33 @@ do
 		then
 			echo "Link was not found"
 		fi
+
+		dir=$(dirname $(echo $line | cut -d' ' -f1))
+
+		if [[ ! -d $dir ]]
+		then
+			echo "Dir not exists"
+		fi
+		
+		new_name=$(basename $(echo $line | cut -d' ' -f1))
+		if [[ -f $(echo $line | cut -d' ' -f1) ]]
+                then
+                        echo "File already exists"
+                        echo "Do you want to rename? y/n "
+                        read choice
+			if [[ $choice == "y" ]]
+			then
+				echo "Enter new name"
+				read new_name
+			fi
+			while [[ -f "$dir/$new_name" ]]
+			do
+                        	echo "File already exists, enter new name"
+				read new_name
+			done
+                fi
+
+		mv $(echo $line | cut -d' ' -f2) "$dir/$new_name"
 	
 		break
 	fi
